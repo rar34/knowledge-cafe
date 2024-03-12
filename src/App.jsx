fb82a02e9ffeb5a521ac540c99480bfe5a51308f
rar34 <ra.cse34@gmail.com>
2024-03-12 22:41:01 +0600

    created react file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-6xl font-bold text-red-500 text-center'>Knowledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
